1d4c59b3ee684fede231582006f49ff7928020e5
deanrivers <deanriversdeveloper@gmail.com>
2020-08-04 11:29:45 -0400

    Updates

---
+++ precache-manifest.e593d97361bc245b4e32efe6828c5431.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780660f36f308bb52e3c95ad00bedcbc",
+    "url": "/2020_Website/index.html"
+  },
+  {
+    "revision": "94e65d82237ef2b7d205",
+    "url": "/2020_Website/static/css/2.1a0a2c47.chunk.css"
+  },
+  {
+    "revision": "0314b92db9a47eb7d98a",
+    "url": "/2020_Website/static/css/main.03ca5f78.chunk.css"
+  },
+  {
+    "revision": "94e65d82237ef2b7d205",
+    "url": "/2020_Website/static/js/2.4e04979f.chunk.js"
+  },
+  {
+    "revision": "06fe01536afcb41310a67e8887f6d0c2",
+    "url": "/2020_Website/static/js/2.4e04979f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0314b92db9a47eb7d98a",
+    "url": "/2020_Website/static/js/main.a8ac7a2e.chunk.js"
+  },
+  {
+    "revision": "e7c764d275f97b16a4df",
+    "url": "/2020_Website/static/js/runtime-main.8c5abe3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2020_Website/static/media/logo.5d5d9eef.svg"
+  }
+]);
